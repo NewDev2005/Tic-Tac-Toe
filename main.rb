453fb6bf75d18c6ab46ca959ef7281d8c0602e30
krypto <clayjeansen3@gmail.com>
2024-05-29 14:47:40 +0530

    game board added

--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 def game_board
-   for i in (0...3)
-    puts "  |  |  "
+   for i in (0...5)
+    if i.odd? 
+        puts "\u2500\u2500+\u2500\u2500\u2500+\u2500\u2500"
+    else
+        puts "  |   |  "      
+        end
    end
 end
 
