82a1369dcdd65fa417437fb706cdf7d14bb01ccf
krypto <clayjeansen3@gmail.com>
2024-05-30 17:01:21 +0530

    two player object added

--- main.rb
+++ main.rb
@@ -1,11 +1,20 @@
-def game_board
+class Players
+    
+end
+
+player1 = Players.new()
+player2 = Players.new()
+
+def game_board(first,position)
    for i in (0...5)
     if i.odd? 
         puts "\u2500\u2500+\u2500\u2500\u2500+\u2500\u2500"
+    elsif i == position
+        puts "x | #{first} | x "
     else
-        puts "  |   |  "      
+        puts "  | #{first} |  "      
         end
    end
 end
 
-game_board()
+game_board('O',4)
