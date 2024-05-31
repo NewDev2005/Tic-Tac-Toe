e142fe12852c3b6258e93527873159549098f938
krypto <clayjeansen3@gmail.com>
2024-05-31 16:43:52 +0530

    New board created with arg var as placeholder

--- main.rb
+++ main.rb
@@ -1,20 +1,20 @@
-class Players
-    
-end
+def game_board(*args)  #Board for showcase
 
-player1 = Players.new()
-player2 = Players.new()
+   first_row = "#{args[0]} | #{args[1]} | #{args[2]}"
+   horiz_line1 = "\u2500\u2500+\u2500\u2500\u2500+\u2500\u2500"
+   second_row = "#{args[3]} | #{args[4]} | #{args[5]}" 
+   horiz_line12 = "\u2500\u2500+\u2500\u2500\u2500+\u2500\u2500"
+   third_row =    "#{args[6]} | #{args[7]} | #{args[8]}" 
+   
+    puts first_row
+    puts horiz_line1
+    puts second_row
+    puts horiz_line12
+    puts third_row
+ end
 
-def game_board(first,position)
-   for i in (0...5)
-    if i.odd? 
-        puts "\u2500\u2500+\u2500\u2500\u2500+\u2500\u2500"
-    elsif i == position
-        puts "x | #{first} | x "
-    else
-        puts "  | #{first} |  "      
-        end
-   end
-end
 
-game_board('O',4)
+
+
+
+ game_board(1,2,3,4,5,6,7,8,9)
